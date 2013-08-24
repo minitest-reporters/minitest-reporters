36e3f59bdfb800fd1140794f3629aaa8e9ef15a3
Adam Spiers <minitest-reporters@adamspiers.org>
2013-08-24 15:39:22 +0100

    Guard::MinitestNotifier was renamed to Guard::Minitest::Notifier (fixes #79)

--- lib/minitest/reporters/guard_reporter.rb
+++ lib/minitest/reporters/guard_reporter.rb
@@ -10,11 +10,22 @@ module MiniTest
     class GuardReporter
       include Reporter
 
+      def notifier_class
+        if ::Guard.const_defined? "MinitestNotifier"
+          # old guard-minitest API prior to 1.0.0.beta.2
+          ::Guard::MinitestNotifier
+        elsif ::Guard.const_defined? "Minitest" and ::Guard::Minitest.const_defined? "Notifier"
+          # new guard-minitest API, by 02b46ee in between
+          # 1.0.0.beta1 and 1.0.0.beta.2
+          ::Guard::Minitest::Notifier
+        end
+      end
+
       def after_suites(*args)
         duration = Time.now - runner.suites_start_time
-        ::Guard::MinitestNotifier.notify(runner.test_count, runner.assertion_count,
-                                         runner.failures, runner.errors,
-                                         runner.skips, duration)
+        notifer_class.notify(runner.test_count, runner.assertion_count,
+                             runner.failures, runner.errors,
+                             runner.skips, duration)
       end
     end
   end
