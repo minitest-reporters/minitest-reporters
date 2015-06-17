9f8959a32eee1f5419e7951aaecd11cdb3fbce1f
Michael <mmcwilliam@gmail.com>
2015-06-17 13:51:24 -0600

    Accept color output in travis-ci test results
    Travis CI runs tests on xterm, which is a colour enabled terminal. Travis CI itself doesn't support color as it pipes the output into a file. The fix is to accept ansi color in the result output.

--- test/integration/reporters/progress_reporter_test.rb
+++ test/integration/reporters/progress_reporter_test.rb
@@ -6,17 +6,17 @@ module MinitestReportersTest
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'progress_test.rb')
       output = `ruby #{test_filename} 2>&1`
-      assert_match 'ERROR["test_error"', output, 'Errors should be displayed'
-      assert_match 'FAIL["test_failure"', output, 'Failures should be displayed'
-      assert_match 'SKIP["test_skip', output, 'Skipped tests should be displayed'
+      assert_match /ERROR(\033\[0m)*\["test_error"/, output, 'Errors should be displayed'
+      assert_match /FAIL(\033\[0m)*\["test_failure"/, output, 'Failures should be displayed'
+      assert_match /SKIP(\033\[0m)*\["test_skip"/, output, 'Skipped tests should be displayed'
     end
     def test_skipped_tests_are_not_displayed
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'progress_detailed_skip_test.rb')
       output = `ruby #{test_filename} 2>&1`
-      assert_match 'ERROR["test_error"', output, 'Errors should be displayed'
-      assert_match 'FAIL["test_failure"', output, 'Failures should be displayed'
-      refute_match 'SKIP["test_skip', output, 'Skipped tests should not be displayed'
+      assert_match /ERROR(\033\[0m)*\["test_error"/, output, 'Errors should be displayed'
+      assert_match /FAIL(\033\[0m)*\["test_failure"/, output, 'Failures should be displayed'
+      refute_match /SKIP(\033\[0m)*\["test_skip"/, output, 'Skipped tests should not be displayed'
     end
   end
 end
