887bf9fc28d9df548146e799bf99a1efe8764fdc
Oleg Sukhodolsky <os97673@gmail.com>
2018-08-25 00:50:21 -0700

    Added support for jruby

--- test/integration/reporters/progress_reporter_test.rb
+++ test/integration/reporters/progress_reporter_test.rb
@@ -5,7 +5,7 @@ module MinitestReportersTest
     def test_all_failures_are_displayed
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'progress_test.rb')
-      output = `ruby #{test_filename} 2>&1`
+      output = `#{ruby_executable} #{test_filename} 2>&1`
       assert_match 'ERROR["test_error"', output, 'Errors should be displayed'
       assert_match 'FAIL["test_failure"', output, 'Failures should be displayed'
       assert_match 'SKIP["test_skip', output, 'Skipped tests should be displayed'
@@ -13,7 +13,7 @@ module MinitestReportersTest
     def test_skipped_tests_are_not_displayed
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'progress_detailed_skip_test.rb')
-      output = `ruby #{test_filename} 2>&1`
+      output = `#{ruby_executable} #{test_filename} 2>&1`
       assert_match 'ERROR["test_error"', output, 'Errors should be displayed'
       assert_match 'FAIL["test_failure"', output, 'Failures should be displayed'
       refute_match 'SKIP["test_skip', output, 'Skipped tests should not be displayed'
@@ -21,14 +21,20 @@ module MinitestReportersTest
     def test_progress_works_with_filter_and_specs
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'spec_test.rb')
-      output = `ruby #{test_filename} -n /length/ 2>&1`
+      output = `#{ruby_executable} #{test_filename} -n /length/ 2>&1`
       refute_match '0 out of 0', output, 'Progress should not puts a warning'
     end
     def test_progress_works_with_strict_filter
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'spec_test.rb')
-      output = `ruby #{test_filename} -n /^test_0001_works$/ 2>&1`
+      output = `#{ruby_executable} #{test_filename} -n /^test_0001_works$/ 2>&1`
       refute_match '0 out of 0', output, 'Progress should not puts a warning'
+    end
+
+    private
+
+    def ruby_executable
+      defined?(JRUBY_VERSION) ? 'jruby' : 'ruby'
     end
   end
 end
