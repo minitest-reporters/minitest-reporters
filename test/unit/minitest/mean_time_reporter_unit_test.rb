minitest-reporters/minitest-reporters

commit 271141c2aaf26ed60db42e9daf57a88dcf35ac66

Need to run on Windows

lib/minitest/reporters/mean_time_reporter.rb
@@ -1,4 +1,5 @@
 require 'minitest/reporters'
+require 'tmpdir'
 require 'yaml'
 
 module Minitest
@@ -125,8 +126,8 @@ module Minitest
           show_progress:          true,
           show_all_runs:          true,
           sort_column:            :avg,
-          previous_runs_filename: '/tmp/minitest_reporters_previous_run',
-          report_filename:        '/tmp/minitest_reporters_report',
+          previous_runs_filename: Dir.tmpdir + '/minitest_reporters_previous_run',
+          report_filename:        Dir.tmpdir + 'minitest_reporters_report'
         }
       end
 

commit e0c62f7aa1d783c18b5dc7e570a70b51e7a0ccd5

add failing test
it seems that 271141c2aaf26ed60db42e9daf57a88dcf35ac66 introduced a small
bug, for example:

...minitest-reporters-1.3.2/lib/minitest/reporters/mean_time_reporter.rb:287:
in `write': Permission denied @ rb_sysopen - /tmpminitest_reporters_report (Errno::EACCES)

test/unit/minitest/mean_time_reporter_unit_test.rb
@@ -20,6 +20,14 @@ module MinitestReportersTest
       File.delete(@report_file_path) if File.exist?(@report_file_path)
     end
 
+    def test_defaults
+      subject = Minitest::Reporters::MeanTimeReporter.new.send(:defaults)
+
+      expected_prefix = "#{Dir.tmpdir}#{File::Separator}"
+      assert_match expected_prefix, subject[:previous_runs_filename]
+      assert_match expected_prefix, subject[:report_filename]
+    end
+
     def test_sorts_avg_numerically
       prev_output = generate_report(:avg, :prev_time)
       report_output = generate_report(:avg, :cur_time)